7f7384f2d6d9a2286bbb6a42ccee5946993a3deb
Jessica Rodriguez <jrodri@redhat.com>
2026-03-28 01:04:02 -0400

    Update test cases for Valkey, except ValkeyServiceTest

---
+++ engine/src/test/java/com/redhat/cloud/notifications/events/ValkeyServiceTest.java
@@ -0,0 +1,12 @@
+package com.redhat.cloud.notifications.events;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+
+@QuarkusTest
+class ValkeyServiceTest {
+    @Inject
+    ValkeyService valkeyService;
+
+    // TODO add client library-specific test cases
+}
